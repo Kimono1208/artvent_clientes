13190b63b700a41ca137feb3806869715f5665ac
Diebarix <40311262+diebarix@users.noreply.github.com>
2024-07-10 19:17:15 -0600

    Google login added

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,32 @@
 <?php
 
 use App\Http\Controllers\GaleriaToldosController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::view('/', 'welcome');
 // Route::view('/galeria', 'cliente/gal');
+
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->stateless()->user();
+
+    $user = User::updateOrCreate([
+        'google_id' => $user_google->id,
+    ], [
+        'name' => $user_google->name,
+        'email' => $user_google->email,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+});
 
 Route::resource('galeria', GaleriaToldosController::class);
 
